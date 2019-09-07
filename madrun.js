149576c9dea424f8ba89e9be75f2e467a794369e
coderaiser <coderaiser@cloudcmd.io>
2019-09-08 00:04:37 +0300

    chore(nupdate) lint

--- madrun.js
+++ madrun.js
@@ -3,11 +3,8 @@
 const {run} = require('madrun');
 
 module.exports = {
-    'lint': () => 'eslint bin lib test madrun.js',
+    'lint': () => 'putout bin lib test madrun.js',
     'fix:lint': () => run('lint', '--fix'),
-    'lint:eslint:bin': () => 'eslint --rule \'no-console:0\' bin',
-    'test': () => 'tape test/*.js',
-    'coverage': () => 'nyc npm test',
     'report': () => 'nyc report --reporter=text-lcov | coveralls',
     'watcher': () => 'nodemon -w test -w lib --exec',
     'watch:test': () => run('watcher', 'npm test'),
